7c2e00754512a8e0c3219eac07107a1dfa9f3b51
andrewfrost2612 <andrew.frost2612@gmail.com>
2022-08-22 12:03:58 +0300

    changes1

--- framework/course.ts
+++ framework/course.ts
@@ -9,6 +9,16 @@ class coursePage {
     mainPlayButton: Locator;
     smallPlayButton: Locator;
     volumeButton: Locator;
+    settingsButton: Locator;
+    fullscreenButton: Locator;
+    showcontentButton1: Locator;
+    showcontentButton2: Locator;
+    showcontentButton3: Locator;
+    showcontentButton4: Locator;
+    showcontentButton5: Locator;
+    showcontentButton6: Locator;
+    getstartednowButton: Locator;
+
 
     constructor(page:Page) {
         this.page = page;
@@ -17,6 +27,15 @@ class coursePage {
         this.mainPlayButton = page.locator('#w-vulcan-v2-45 >> nth=0');
         this.smallPlayButton = page.locator('#w-vulcan-v2-45 > div.w-bottom-bar.w-css-reset > div > div.w-bottom-bar-left.w-css-reset > div > div > div');
         this.volumeButton = page.locator('#w-vulcan-v2-45 > div.w-bottom-bar.w-css-reset > div > div.w-bottom-bar-right.w-css-reset > div.w-bottom-bar-right-inner-anchor.w-css-reset > div > div:nth-child(1) > div > button > div');
+        this.settingsButton = page.locator('#w-vulcan-v2-45 > div.w-bottom-bar.w-css-reset > div > div.w-bottom-bar-right.w-css-reset > div.w-bottom-bar-right-inner-anchor.w-css-reset > div > div:nth-child(3) > div > button > div');
+        this.fullscreenButton = page.locator('#w-vulcan-v2-45 > div.w-bottom-bar.w-css-reset > div > div.w-bottom-bar-right.w-css-reset > div.w-bottom-bar-right-inner-anchor.w-css-reset > div > div:nth-child(4) > div > button > div');
+        this.showcontentButton1 = page.locator('[aria-label="toggle curriculum content"] >> nth=0');
+        this.showcontentButton2 = page.locator('[aria-label="toggle curriculum content"] >> nth=1');
+        this.showcontentButton3 = page.locator('');
+        this.showcontentButton4 = page.locator('');
+        this.showcontentButton5 = page.locator('');
+        this.showcontentButton6 = page.locator('');
+        this.getstartednowButton = page.locator('');
 }
 }
 
--- tests/course.spec.ts
+++ tests/course.spec.ts
@@ -2,7 +2,7 @@ import { test , expect, chromium, firefox } from '@playwright/test'
 import { coursePage } from '../framework/course'
 
 
-test.only('videoplayer', async ({page}) => {
+test('videoplayer', async ({page}) => {
   const course = new coursePage(page);
   await page.goto('https://courses.ultimateqa.com/courses/react-and-nodejs');
   await page.waitForTimeout(5000);
@@ -16,3 +16,15 @@ test.only('videoplayer', async ({page}) => {
   await page.waitForTimeout(2000);
   await course.volumeButton.click();
 });
+
+test('contentdisplaying', async ({page}) => {
+  const course = new coursePage(page);
+  await page.goto('https://courses.ultimateqa.com/courses/react-and-nodejs');
+  await course.showcontentButton1.scrollIntoViewIfNeeded();
+  await course.showcontentButton1.click();
+  await page.waitForTimeout(2000);
+  await course.showcontentButton2.click();
+
+})
+
+
